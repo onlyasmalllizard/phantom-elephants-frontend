04eabb63c7810dbb36dbe36d6be04d01bff2b908
perrettJ4 <perrettj4@gmail.com>
2021-10-25 17:22:04 +0100

    changed table, added bootcamp dropdown to the line graph

--- src/components/TableCard.js
+++ src/components/TableCard.js
@@ -1,12 +1,13 @@
-import Card from '@material-tailwind/react/Card';
-import CardHeader from '@material-tailwind/react/CardHeader';
-import CardBody from '@material-tailwind/react/CardBody';
-import Image from '@material-tailwind/react/Image';
-import Progress from '@material-tailwind/react/Progress';
-import Team1 from '../assets/img/team-1-800x800.jpg';
-import Team2 from '../assets/img/team-2-800x800.jpg';
-import Team3 from '../assets/img/team-3-800x800.jpg';
-import Team4 from '../assets/img/team-4-470x470.png';
+import Card from "@material-tailwind/react/Card";
+import CardHeader from "@material-tailwind/react/CardHeader";
+import CardBody from "@material-tailwind/react/CardBody";
+import Image from "@material-tailwind/react/Image";
+import Progress from "@material-tailwind/react/Progress";
+import Team1 from "../assets/img/team-1-800x800.jpg";
+import Team2 from "../assets/img/team-2-800x800.jpg";
+import Team3 from "../assets/img/team-3-800x800.jpg";
+import Team4 from "../assets/img/team-4-470x470.png";
+import dummyData from "dummyData";
 
 export default function CardTable() {
   return (
@@ -19,148 +20,88 @@ export default function CardTable() {
           <table className="items-center w-full bg-transparent border-collapse">
             <thead>
               <tr>
-                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                  Project
+                <th className="w-32 px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                  Bootcamper
                 </th>
                 <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                  Budget
+                  Bootcamp
                 </th>
                 <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                  Status
+                  Star Rating
                 </th>
                 <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                  Users
+                  Recap Tasks
                 </th>
                 <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                  Completion
+                  Workshops Avg
+                </th>
+                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                  Quiz Avg
                 </th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  Argon Design System
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  $2,500 USD
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <i className="fas fa-circle fa-sm text-orange-500 mr-2"></i>{' '}
-                  pending
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <div className="flex">
-                    <div className="w-10 h-10 rounded-full border-2 border-white">
-                      <Image src={Team1} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team2} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team3} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team4} rounded alt="..." />
-                    </div>
-                  </div>
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <Progress color="red" value="60" />
-                </th>
-              </tr>
-              <tr>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  Black Dashboard Sketch
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  $1,800 USD
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <i className="fas fa-circle fa-sm text-blue-gray-900 mr-2"></i>{' '}
-                  completed
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <div className="flex">
-                    <div className="w-10 h-10 rounded-full border-2 border-white">
-                      <Image src={Team1} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team2} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team3} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team4} rounded alt="..." />
-                    </div>
-                  </div>
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <Progress color="green" value="100" />
-                </th>
-              </tr>
-              <tr>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  React Material Dashboard
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  $4,400 USD
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <i className="fas fa-circle fa-sm text-teal-500 mr-2"></i> on
-                  schedule
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <div className="flex">
-                    <div className="w-10 h-10 rounded-full border-2 border-white">
-                      <Image src={Team1} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team2} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team3} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team4} rounded alt="..." />
-                    </div>
-                  </div>
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <Progress color="teal" value="90" />
-                </th>
-              </tr>
-              <tr>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  React Material Dashboard
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  $2,200 USD
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <i className="fas fa-circle fa-sm text-blue-gray-900 mr-2"></i>{' '}
-                  completed
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <div className="flex">
-                    <div className="w-10 h-10 rounded-full border-2 border-white">
-                      <Image src={Team1} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team2} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team3} rounded alt="..." />
-                    </div>
-                    <div className="w-10 h-10 rounded-full border-2 border-white -ml-4">
-                      <Image src={Team4} rounded alt="..." />
-                    </div>
-                  </div>
-                </th>
-                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                  <Progress color="green" value="100" />
-                </th>
-              </tr>
+              {/* got through all work, if recapData,  */}
+              {dummyData[0].students.map((student) => {
+                /* creates an array without nulls, then counts the occurences, output => Object */
+
+                const recapTasks = student.work
+                  .reduce(
+                    (acc, cur) =>
+                      cur.recapTask ? [...acc, cur.recapTask.score] : acc,
+                    []
+                  )
+                  .reduce(function (acc, curr) {
+                    return acc[curr] ? ++acc[curr] : (acc[curr] = 1), acc;
+                  }, {});
+
+                console.log(recapTasks);
+                return (
+                  <>
+                    <tr className="border-b border-gray-200">
+                      <div className="flex">
+                        <th className="flex align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left items-center">
+                          <div className="w-10 h-10 rounded-full border-2 border-white ">
+                            <Image
+                              src={student.info.avatar}
+                              rounded
+                              alt="student avatar"
+                            />
+                          </div>
+
+                          <p className="font-light text-sm whitespace-nowrap px-2 py-4 pl-3 text-left ">
+                            {" "}
+                            {student.info.name}
+                          </p>
+                        </th>
+                      </div>
+                      <th className="font-light text-sm whitespace-nowrap px-2 py-4 text-left ">
+                        {dummyData[0].id}
+                      </th>
+
+                      <th className="font-light text-sm whitespace-nowrap px-2 py-4 text-left ">
+                        placeholder
+                      </th>
+                      <th className="font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                        <div className="flex content-evenly">
+                          <div className="bg-green-500">
+                            {" "}
+                            <p className="">{recapTasks.green}</p>
+                          </div>
+                          <div className="bg-orange-500">
+                            {" "}
+                            <p>{recapTasks.amber}</p>
+                          </div>
+                          <div className="bg-red-500">
+                            {" "}
+                            <p>{recapTasks.red}</p>
+                          </div>
+                        </div>
+                      </th>
+                    </tr>
+                  </>
+                );
+              })}
             </tbody>
           </table>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    mode: 'jit',
-    purge: ['./src/**/*.js', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        fontFamily: {
-            sans: ['Roboto', 'sans-serif'],
-            serif: ['"Roboto Slab"', 'serif'],
-            body: ['Roboto', 'sans-serif'],
-        },
-        extend: {},
+  mode: "jit",
+  purge: ["./src/**/*.js", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      serif: ['"Roboto Slab"', "serif"],
+      body: ["Roboto", "sans-serif"],
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
