a241bf5ca56783af492bf4934594dbd0e701b0e8
perrettJ4 <perrettj4@gmail.com>
2021-11-02 19:13:31 +0000

    made a fetch to localhost and all is okay

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Dashboard from './pages/Dashboard';
-import Settings from './pages/Settings';
-import Tables from './pages/Tables';
-import Maps from './pages/Maps';
-import Footer from './components/Footer';
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import Tables from "./pages/Tables";
+import Maps from "./pages/Maps";
+import Footer from "./components/Footer";
 
 // Tailwind CSS Style Sheet
-import 'assets/styles/tailwind.css';
+import "assets/styles/tailwind.css";
 
 function App() {
+  const [cohortData, setCohortData] = useState({});
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("http://localhost:3000/records", {
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      return data.payload;
+    }
+    let data = getData();
+    setCohortData(data);
+  }, []);
+  console.log(cohortData);
+
   return (
     <>
       <Sidebar />
