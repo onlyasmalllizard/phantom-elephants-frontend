e6878f07f284e982a8b6a3fece60cc9e229bdf39
perrettJ4 <perrettj4@gmail.com>
2021-11-08 10:03:47 +0000

    fixed sidebar hiding issue - tailwind changes have not saved but will redo them

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
   ) : (
     <UserProvider>
       <Sidebar setPushRight={setPushRight} pushRight={pushRight} />
-      <div className={pushRight ? "ml-64" : ""}>
+      <div className={`${pushRight ? "ml-64 " : ""} xl:ml-64`}>
         <Switch>
           <Route exact path="/">
             <Dashboard
