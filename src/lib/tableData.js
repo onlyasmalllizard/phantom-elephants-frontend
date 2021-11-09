818f54fde064ee6e8155ef02e9574ef0cb0c1ca1
Lizard Morrow <lizardmorrow@gmail.com>
2021-11-09 10:17:40 +0000

    Fix import in tableData

--- src/lib/tableData.js
+++ src/lib/tableData.js
@@ -1,33 +1,33 @@
-import bootcamps from "../dummyData";
+import * as bootcamps from '../dummyData';
 
 export const tableHeaders = [
-  { display: "Bootcamper", id: "name" },
-  { display: "Bootcamp", id: "bootcampId" },
-  { display: "Region", id: "bootcampRegion" },
-  { display: "Recap Tasks", id: "avgRecapScore" },
-  { display: "Workshops", id: "avgWorkshopScore" },
-  { display: "Quiz Avg %", id: "avgQuiz" },
-  { display: "Mood Avg /5", id: "avgExperience" },
+  { display: 'Bootcamper', id: 'name' },
+  { display: 'Bootcamp', id: 'bootcampId' },
+  { display: 'Region', id: 'bootcampRegion' },
+  { display: 'Recap Tasks', id: 'avgRecapScore' },
+  { display: 'Workshops', id: 'avgWorkshopScore' },
+  { display: 'Quiz Avg %', id: 'avgQuiz' },
+  { display: 'Mood Avg /5', id: 'avgExperience' },
 ];
 export const viewOptions = [
-  "All Students",
-  ...bootcamps.map((bootcamp) => "Bootcamp " + bootcamp.id),
+  'All Students',
+  ...bootcamps.map((bootcamp) => 'Bootcamp ' + bootcamp.id),
   ...bootcamps.map((bootcamp) => bootcamp.region),
 ];
 
 export const filterOptions = {
-  0: "all",
-  1: "bootcampId",
-  2: "bootcampId",
-  3: "bootcampId",
-  4: "bootcampId",
-  5: "bootcampRegion",
-  6: "bootcampRegion",
-  7: "bootcampRegion",
-  8: "bootcampRegion",
+  0: 'all',
+  1: 'bootcampId',
+  2: 'bootcampId',
+  3: 'bootcampId',
+  4: 'bootcampId',
+  5: 'bootcampRegion',
+  6: 'bootcampRegion',
+  7: 'bootcampRegion',
+  8: 'bootcampRegion',
 };
 export const filtationMethod = [
-  "all",
+  'all',
   1,
   2,
   3,
