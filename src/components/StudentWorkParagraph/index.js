08cd2e1f7aa29bc25858fc54ff7b54d362304ad4
Lizard Morrow <lizardmorrow@gmail.com>
2021-11-10 11:15:30 +0000

    Fix footer display

--- src/components/StudentWorkParagraph/index.js
+++ src/components/StudentWorkParagraph/index.js
@@ -1,18 +1,22 @@
-import Paragraph from "@material-tailwind/react/Paragraph";
-import uuid from "react-uuid";
+import Paragraph from '@material-tailwind/react/Paragraph';
+import uuid from 'react-uuid';
 
 const StudentWorkParagraph = ({
   workArray,
   doesntExistMessage,
   labels = [],
 }) => {
-  console.log("feedbcak work array:", workArray);
+  console.log('feedbcak work array:', workArray);
   return (
     <section className="mt-8">
       {workArray.map((item, index) => (
         <Paragraph key={uuid()}>
           <div className="flex flex-col h-20">
-            {<b>{labels[index]}</b>}
+            {
+              <a href="https://github.com/onlyasmalllizard/phantom-elephants-frontend/tree/james-branch-details">
+                {labels[index]}
+              </a>
+            }
 
             {item ? item : doesntExistMessage}
           </div>
