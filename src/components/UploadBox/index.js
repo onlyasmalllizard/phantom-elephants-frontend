163e3d181101ac76b238df31f885433f3d63ebc8
perrettJ4 <perrettj4@gmail.com>
2021-11-11 16:34:21 +0000

    added img back to upload boxes

--- src/components/UploadBox/index.js
+++ src/components/UploadBox/index.js
@@ -4,11 +4,13 @@ import Card from "@material-tailwind/react/Card";
 import CardHeader from "@material-tailwind/react/CardHeader";
 import CardBody from "@material-tailwind/react/CardBody";
 import CardFooter from "@material-tailwind/react/CardFooter";
+import InputIcon from "@material-tailwind/react/InputIcon";
+import H6 from "@material-tailwind/react/Heading5";
 import Button from "@material-tailwind/react/Button";
 
 export default class CSVReader2 extends Component {
   postcsv = async (data) => {
-    const response = await fetch(process.env.REACT_APP_API_URL + "/students", {
+    const response = await fetch("http://localhost:3000/students", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -46,11 +48,27 @@ export default class CSVReader2 extends Component {
           </CardHeader>
           <CardBody className="-py-0">
             <div style={{ textAlign: "left" }} className="mt-4 mb-8 px-4 ">
+              <div className="flex justify-center items-center mt-0 mb-12">
+                <img
+                  alt={this.props.imgAlt}
+                  src={this.props.imgSrc}
+                  style={{
+                    height: "12rem",
+                    width: "auto",
+                    borderRadius: "1rem",
+                    border: `2px ${this.props.headerColor} solid`,
+                  }}
+                />
+              </div>
               <CSVReader
                 onDrop={this.handleOnDrop}
                 onError={this.handleOnError}
                 addRemoveButton
                 onRemoveFile={this.handleOnRemoveFile}
+                color={this.props.headerColor}
+                style={{
+                  borderColor: this.props.headerColor,
+                }}
               >
                 <span>Drop CSV file here or click to upload.</span>
               </CSVReader>
